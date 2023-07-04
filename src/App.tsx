8266b37a57c29910c3f5d4c639c4843773ba4308
Grigory Dobrenko <grigorydobrenko97@gmail.com>
2023-07-04 21:18:16 +0300

    feat: stylize button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import { Button } from './components/ui/button'
-import { Typography } from './components/ui/typography/typography.tsx'
 
 export function App() {
   return (
     <div>
-      <Button as={'a'} href={'/link'}>
-        Button
-      </Button>
-      <Typography as={'a'} href={'google.com'} variant={'large'}>
-        Exapmle
-      </Typography>
+      <Button>Button</Button>
     </div>
   )
 }
